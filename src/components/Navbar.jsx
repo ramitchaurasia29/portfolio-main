e6762d8e5e9d721bd734a4478ac3ef4a824f1329
ramit <ramit2900@gmail.com>
2024-01-30 14:59:19 +0530

    unusual commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
   <div className="w-full  flex flex-wrap items-center justify-between  p-4 pr-8">
     <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse rounded-3xl">
         <img src="/images/face.jpg" className="h-12 w-12 rounded-full" alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Thumbstack</span>
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Portfolio</span>
     </a>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
